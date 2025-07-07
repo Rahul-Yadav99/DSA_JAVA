ce99f8beb1a2a4e512a0659db29cef982cdc77e5
rahul <rahul352sj@gmail.com>
2025-07-07 12:54:29 +0530

    first commit

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ if (typeof userData.age == "number") {
 } else {
     console.log("age is not a number")
 }
+
+var a = 1
+var b = 12
+if (a == 1 || b == 1) {
+    console.log("a and b are equal")
+} else {
+    console.log("a and b are not equal")
+}
