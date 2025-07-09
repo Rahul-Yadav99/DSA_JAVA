da83c305f9d6ceba826cb61c766032591e446018
rahul <rahul352sj@gmail.com>
2025-07-09 14:55:52 +0530

    compleate string topic in java

---
+++ countVowelsInString.java
@@ -0,0 +1,17 @@
+public class countVowelsInString {
+    public static void main(String[] args) {
+        String str = "hello world";
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = Character.toLowerCase(str.charAt(i));
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        if (count > 0) {
+            System.out.println("Total vowels " + count);
+        } else {
+            System.out.println("No Vowels are present in this given String");
+        }
+    }
+}
---
+++ subString.java
@@ -0,0 +1,15 @@
+public class subString {
+    public static String subStringg(String str, int si, int ei) {
+        String subStr = "";
+        for (int i = si; i < ei; i++) {
+            subStr = subStr + str.charAt(i);
+        }
+        return subStr;
+    }
+
+    public static void main(String[] args) {
+        String str = "HelloWorld";
+        System.out.println(str.substring(0, 5));
+        System.out.println(subStringg(str, 0, 7));
+    }
+}
