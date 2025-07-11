b0afa483c6899f5a57f85092303636856ce23f7b
rahul <rahul352sj@gmail.com>
2025-07-11 20:52:16 +0530

    finally recusion is complete

---
+++ printXn.java
@@ -0,0 +1,9 @@
+public class printXn {
+    public static void main(String[] args) {
+        int res = 1;
+        for (int i = 1; i <= 10; i++) {
+            res = 2 * res;
+        }
+        System.out.println(res);
+    }
+}
