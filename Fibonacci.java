9b808345732fdeb5aa414ba9a2e1ffa9946167d6
rahul <rahul352sj@gmail.com>
2025-07-21 09:34:09 +0530

    kiya dekh raha hai, bhai!!

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,12 +1,12 @@
 public class Fibonacci {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 10;
         int x = 14;
         String str = "rahul";
         int result = fib(x);
         System.out.println("Fibonacci number at position " + x + " is: " + result);
         int sum = sumOfDigit(n);
-        System.out.println(sum);
+        System.out.println("sum " + sum);
         String res = revString(str);
         System.out.println(res);
         int findPower = powerOfN(10);
@@ -15,7 +15,10 @@ public class Fibonacci {
         System.out.println();
         fun(n);
         System.out.println();
-        printMulti(2, 10);
+        printMulti(5, 1);
+        System.out.println();
+        int al = alternateSigns(n);
+        System.out.println(al);
     }
 
     public static int fib(int n) {
@@ -75,11 +78,23 @@ public class Fibonacci {
 
     // 12 24 36 48 60
     public static void printMulti(int num, int k) {
-        if (num == 0 || k == 0) {
+        if (k == 0) {
             return;
         }
         printMulti(num, k - 1);
         System.out.print((num * k) + " ");
     }
 
+    public static int alternateSigns(int n) {
+        // 1-2+3-4+5 = 3
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n % 2 == 0) {
+            return alternateSigns(n - 1) - n;
+        } else {
+            return alternateSigns(n - 1) + n;
+        }
+    }
 }
