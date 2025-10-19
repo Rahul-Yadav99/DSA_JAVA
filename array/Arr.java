9dd5314cb08dfdd923c66b3cca761627bb2541b9
Rahul <rahul352sj@gmail.com>
2025-10-20 00:34:58 +0530

    lets gooooo....

---
+++ array/Arr.java
@@ -0,0 +1,11 @@
+
+public class Arr {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        for (int number : numbers) {
+            System.out.print(number);
+            System.out.print(" ");
+        }
+    }
+}
