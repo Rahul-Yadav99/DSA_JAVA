d23ce3c2292531aaf273e21367e228f6db221b51
Rahul <rahul352sj@gmail.com>
2025-10-26 12:56:32 +0530

    new logic added for price estimation in new logic we created a new feild in vehicle modal estimationPricePerKgKm it is different from pricePerKgPerKm in new field we add the small value for minimum price estimation

--- array/Arr.java
+++ array/Arr.java
@@ -1,8 +1,19 @@
 public class Arr {
     public static void main(String[] args) {
-        int n = 10;
-        for (int i = 1; i < n; i++) {
-            System.out.print(i);
+        int n[] = { 1, 2, 4, 5, 3 };
+        for (int i = 0; i < n.length; i++) {
+            for (int j = i + 1; j < n.length; j++) {
+                if (n[i] > n[j]) {
+                    int temp = n[i];
+                    n[i] = n[j];
+                    n[j] = temp;
+                }
+
+            }
+        }
+        System.out.println("Array in descending order:");
+        for (int i = 0; i < n.length; i++) {
+            System.out.print(n[i] + " ");
         }
     }
 }
