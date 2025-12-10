a1987281c7b7da2868c92ca1b8ebc318a288eec4
Rahul <rahul352sj@gmail.com>
2025-12-10 13:11:01 +0530

    searching

--- Problem.java
+++ Problem.java
@@ -32,9 +32,45 @@ public class Problem {
         }
     }
 
+    static void binarySearchonRotatedArr(int[] nums) {
+        int s = 0;
+        int e = nums.length - 1;
+        int x = 5;
+
+        while (s <= e) {
+            int mid = (s + e) / 2;
+
+            if (nums[mid] == x) {
+                System.out.println(mid);
+                return;
+            }
+
+            // Check if left part is sorted
+            if (nums[s] <= nums[mid]) {
+                // Check if x lies in the sorted left part
+                if (x >= nums[s] && x < nums[mid]) {
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
+                }
+            }
+            // Right part is sorted
+            else {
+                // Check if x lies in the sorted right part
+                if (x > nums[mid] && x <= nums[e]) {
+                    s = mid + 1;
+                } else {
+                    e = mid - 1;
+                }
+            }
+        }
+
+        System.out.println("Not Found");
+    }
+
     public static void main(String[] args) {
         // find the min element in this given array and return the index of the element
         int[] nums = { 3, 4, 5, 1, 2 };
-        
+        binarySearchonRotatedArr(nums);
     }
 }
