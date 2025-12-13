a3e2a49215bb50d88b154073d7cb45f910b7f01d
Rahul <rahul352sj@gmail.com>
2025-12-13 14:15:50 +0530

    sorting is done

---
+++ KsubMax.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class KsubMax {
+
+    public static ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        ArrayList<Integer> res = new ArrayList<>();
+
+        if (k == 1) {
+            for (int i : arr) {
+                res.add(i);
+            }
+            return res;
+        }
+
+        for (int i = 0; i <= arr.length - k; i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = i; j < i + k; j++) {
+                max = Math.max(max, arr[j]);
+            }
+            res.add(max);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 1, 4, 5, 2, 3, 6 };
+        int k = 3;
+        System.out.println(maxOfSubarrays(arr, k));
+    }
+}
--- SlidingWindow.java
+++ SlidingWindow.java
@@ -1,18 +1,17 @@
 public class SlidingWindow {
     public static void main(String[] args) {
         int[] arr = { 2, 3, 5, 4, 1 };
-        int msum = Integer.MIN_VALUE;
         int k = 3;
-        for (int i = 0; i <= arr.length - k; i++) {
-            int csum = 0;
-            for (int j = i; j < i + k; j++) {
-                csum += arr[j];
-
-            }
-            if (csum > msum) {
-                msum = csum;
-            }
+        int winSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < k; i++) {
+            winSum = winSum + arr[i];
         }
-        System.out.println(msum);
+        maxSum = winSum;
+        for (int i = k; i < arr.length; i++) {
+            winSum = winSum - arr[i - k] + arr[i];
+            maxSum = Math.max(maxSum, winSum);
+        }
+        System.out.println(maxSum);
     }
 }
