ce150c29fb15ddeafc1d9a1c686a7bc59e0d62cb
rahul <rahul352sj@gmail.com>
2025-12-20 13:17:37 +0530

    hashmap

---
+++ HashMapDemo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "Rahul Yadav");
+        System.out.println(map);
+    }
+}
