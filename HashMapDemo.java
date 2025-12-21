b4d56be09a320464805dc26bc7ae3344527e3d7b
rahul <rahul352sj@gmail.com>
2025-12-21 17:12:23 +0530

    hashmap

--- HashMapDemo.java
+++ HashMapDemo.java
@@ -4,6 +4,14 @@ public class HashMapDemo {
     public static void main(String[] args) {
         HashMap<Integer, String> map = new HashMap<>();
         map.put(1, "Rahul Yadav");
-        System.out.println(map);
+        int arr[] = { 1, 2, 3, 3, 4, 5 };
+        boolean hasDuplicate = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                hasDuplicate = true;
+                break;
+            }
+        }
+        System.out.println(hasDuplicate);
     }
 }
