398497cc530e19a03d104f0a067b5dd80ec4e4f9
Rahul <rahul352sj@gmail.com>
2025-12-23 10:26:13 +0530

    fixed

--- Loop.java
+++ Loop.java
@@ -1,9 +1,9 @@
 public class Loop {
     public static void main(String[] args) {
-        int i;
-        for (i = 9; i < 10; i = i + 1) {
-            System.out.println("rahul");
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j < 4; j++) {
+                // if
+            }
         }
-        System.out.println(i);
     }
 }
