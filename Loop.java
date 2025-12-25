9ffc42a74cd949a84b2e634accd2083f7590a04f
Rahul <rahul352sj@gmail.com>
2025-12-25 14:59:29 +0530

    fixed

--- Loop.java
+++ Loop.java
@@ -1,9 +1,18 @@
 public class Loop {
     public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j < 4; j++) {
-                // if
+        int arr[][] = {
+                { 2, 1, 4 },
+                { 4, 3, 100 }
+        };
+        int x = 100;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == x) {
+                    System.out.println(i + " is row and coloum is " + j);
+                    break;
+                }
             }
+            // System.out.println();
         }
     }
 }
